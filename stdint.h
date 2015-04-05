f9d990a67e6026b05bd22a36dcae254bad053fe0
Leo Cassarani <leo.cassarani@me.com>
2015-04-05 23:08:50 +0000

    Redefine uint32_t as "unsigned long"
    This is so converting from a pointer to a uint32_t becomes easier.

--- stdint.h
+++ stdint.h
@@ -3,9 +3,9 @@
 
 typedef unsigned char          uint8_t;
 typedef unsigned short         uint16_t;
-typedef unsigned int           uint32_t;
+typedef unsigned long          uint32_t;
 typedef unsigned long long int uint64_t;
 
-typedef unsigned long uintptr_t;
+typedef uint32_t uintptr_t;
 
 #endif
